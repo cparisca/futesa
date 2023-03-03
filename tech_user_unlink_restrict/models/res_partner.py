8c19c93eaa51a3c0c97b5b5463781053366f7f0c
Ketan Kachhela <l.kachhela28@gmail.com>
2023-03-03 21:36:49 +0530

    [IMP] Changes

--- tech_user_unlink_restrict/models/res_partner.py
+++ tech_user_unlink_restrict/models/res_partner.py
@@ -3,14 +3,14 @@ from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError, ValidationError
 
 
-class ResUsers(models.Model):
-    _inherit = "res.users"
+class ResPartner(models.Model):
+    _inherit = "res.partner"
 
     def _check_admin_user(self):
         for rec in self:
-            if rec.id in [1, 2]:
+            if rec.id in [1, 2, 3, 4, 5, 6, 7]:
                 raise ValidationError(_("No se pueden borrar los usuarios Admin"))
 
     def unlink(self):
         self._check_admin_user()
-        return super(ResUsers, self).unlink()
+        return super(ResPartner, self).unlink()

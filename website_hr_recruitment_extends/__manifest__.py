c02897146b0730585db3e4189f0009afe2c3e82d
kalim-shaikh <kalimshaikh.cambay@gmail.com>
2023-03-15 11:14:26 +0530

    [ADD]add module website_hr_recruitment_extends to add jobdescription on website page with custom texts

---
+++ website_hr_recruitment_extends/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Website HR Recruitment Extends',
+    'version': '15.0.0',
+    'category': 'Website/Website',
+    'summary': 'Website HR Recruitment Extends',
+    'sequence': '10',
+    'author': '',
+    'license': 'LGPL-3',
+    'company': '',
+    'maintainer': 'Kalim',
+    'support': '',
+    'website': '',
+    'depends': [
+        'website_hr_recruitment',
+    ],
+    'demo': [],
+    'data': [
+        'views/template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'qweb': [],
+}

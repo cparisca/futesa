aafb9633c5c11b7d2921cbbeb8038c5bbd6a357c
kalim-shaikh <kalimshaikh.cambay@gmail.com>
2023-03-16 23:28:15 +0530

    [IMP]changes

--- website_hr_recruitment_extends/__manifest__.py
+++ website_hr_recruitment_extends/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Website HR Recruitment Extends',
-    'version': '15.0.1',
+    'version': '15.0',
     'category': 'Website/Website',
     'summary': 'Website HR Recruitment Extends',
     'sequence': '10',

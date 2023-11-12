6043e9a1bb8236a0176217d2a6f6c7497ce114e3
Darm1640 <93557532+Darm1640@users.noreply.github.com>
2023-11-12 16:37:04 -0500

    Update hr_leave.py

--- lavish_hr_payroll/models/hr_leave.py
+++ lavish_hr_payroll/models/hr_leave.py
@@ -89,6 +89,7 @@ class HolidaysRequest(models.Model):
         for record in self:
             if record.force_ibc and record.ibc != 0:
                 record.payroll_value = (record.ibc / 30) * record.number_of_days
+                record._prepare_leave_line()
             else:
                 record.get_amount_license()
 
